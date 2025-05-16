f4945715604b4679ef84c61d57db17f454f4ab78
dimidroloff <126184245+dimidroloff@users.noreply.github.com>
2025-05-16 14:18:38 +0700

    6

--- 16-05-25/1.py
+++ 16-05-25/1.py
@@ -1,6 +1,12 @@
 # Дано 3 числа. Найти минимальное среди них и вывести на экран.
-a = float(input("Первое число: "))
-b = float(input("Второе число: "))
-c = float(input("Третье число: "))
+a = int(input("Первое число: "))
+b = int(input("Второе число: "))
+c = int(input("Третье число: "))
 
 print("минимальное число:", min(a, b, c))
+if a < b <= c or a < c <= b:
+    print(a)
+elif b < a <= c or b < c <= a:
+    print(b)
+else:
+    print(c)
